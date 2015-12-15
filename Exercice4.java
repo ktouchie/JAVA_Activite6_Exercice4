59f0bd1ed8143e3635763351221029883ee762d8
Karin Touchie <ktouchie@gmail.com>
2015-12-15 11:20:21 +0100

    Create prompts & variables, empty method

---
+++ Exercice4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Exercice4 {
+
+	public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a message:");
+        String msg = (sc.nextLine()).toUpperCase();
+        
+        System.out.println("Enter a value for K:");
+        int k = sc.nextInt();
+
+        
+	}
+
+    public static void {
+        
+    }
+}
